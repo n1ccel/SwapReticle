67dbcd099a650e38332b4306faf987e11d82c3fe
Алексей Королев <korolev.alexeyandreevich93@gmail.com>
2023-01-27 18:03:59 +0400

    Добавьте файлы проекта.

---
+++ MisaMain.cs
@@ -0,0 +1,23 @@
+﻿using ABI_RC.Core.Player;
+using MelonLoader;
+using System;
+using System.Reflection;
+
+namespace Misatyan
+{
+    public class Misa : MelonMod
+    {
+        public static Misa Instance { get; private set; }
+        public MelonLogger.Instance Logger => Instance.LoggerInstance;
+
+        //Swap shit CVR's riticle to good red point
+        [Obsolete]
+        public override void OnApplicationStart()
+        {
+            MelonCoroutines.Start(ReticleSwitch.WaitForMenu());
+            //MelonCoroutines.Start(MainGUI.WaitForMenu());
+            Instance = this;
+        }
+    }
+}
+
---
+++ Reticle/ReticleSwitch.cs
@@ -0,0 +1,19 @@
+﻿using ReticleSW;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Misatyan
+{
+    internal class ReticleSwitch
+    {
+        public static IEnumerator WaitForMenu()
+        {
+            while (!GameObject.Find("Cohtml/CohtmlWorldView")) { yield return new WaitForSeconds(1); }
+            var reticle = GameObject.Find("_PLAYERLOCAL/[CameraRigDesktop]/Camera/Canvas/Image");
+            reticle.GetComponent<Image>().sprite = Asset.Reticle();
+            reticle.GetComponent<Image>().material.shader = Shader.Find("Sprites/Default");
+            reticle.transform.localScale = new Vector2(1, 1);
+        }
+    }
+}
